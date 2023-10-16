96bb04d1151ffc369b82a6d05ec9f0c849831e85
JaMyiusC <jamyius567@gmail.com>
2023-10-16 09:26:39 -0400

    fixed user and manager

--- userManager.java
+++ userManager.java
@@ -1,40 +1,14 @@
-<<<<<<< Updated upstream
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-
-/**
- * The UserManager class is responsible for managing user information.
- */
-public class userManager {
-    private static userManager userManager;
-=======
 import java.util.ArrayList;
 
 public class UserManager {
     private static UserManager userManager;
->>>>>>> Stashed changes
     private ArrayList<User> usersList;
 
     private userManager() {
         this.usersList = new ArrayList<>();
     }
 
-<<<<<<< Updated upstream
-    /**
-     * Gets the instance of the UserManager using the Singleton pattern.
-     *
-     * @return The UserManager instance.
-     */
-    public static userManager getInstance() {
-        if (userManager == null) {
-            userManager = new userManager();
-        }
-=======
-   
     public static UserManager getInstance() {
->>>>>>> Stashed changes
         return userManager;
     }
 
