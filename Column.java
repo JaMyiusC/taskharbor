5acbd2a7e52edf69e2e4e7e3320aaf29c26e9584
Colin Mathis Elmore <colin_elmore@Colins-MacBook-Pro-3.local>
2023-10-16 00:04:18 -0400

    fixed some

--- Column.java
+++ Column.java
@@ -1,70 +1,38 @@
 import java.util.ArrayList;
 
-
 public class Column {
     private String columnName;
     private int columnPosition;
     private ArrayList<Task> columnTaskList;
 
-    /**
-     * Constructor to initialize a new column with a name and position.
-     *
-     * @param columnName     The name of the column.
-     * @param columnPosition The position of the column.
-     */
+    // Constructor to initialize the column name, position, and task list
     public Column(String columnName, int columnPosition) {
         this.columnName = columnName;
         this.columnPosition = columnPosition;
         this.columnTaskList = new ArrayList<>();
     }
 
-    /**
-     * Adds a new task to the column's task list.
-     *
-     * @param taskName The name of the task to add.
-     * @return true if the task was successfully added, false otherwise.
-     */
+    // Add a task to the column's task list
     public boolean addTaskForColumn(String taskName) {
-        Task task = new Task(taskName);
-        return columnTaskList.add(task);
-    }
-
-    /**
-     * Removes a task from the column's task list by its name.
-     *
-     * @param taskName The name of the task to remove.
-     * @return true if the task was successfully removed, false otherwise.
-     */
-    public boolean removeTaskForColumn(String taskName) {
-        Task taskToRemove = null;
-        for (Task task : columnTaskList) {
-            if (task.getTaskName().equals(taskName)) {
-                taskToRemove = task;
-                break;
-            }
-        }
-        if (taskToRemove != null) {
-            return columnTaskList.remove(taskToRemove);
-        }
+        // Placeholder implementation
         return false;
     }
 
-    /**
-     * Get the list of tasks in the column.
-     *
-     * @return An ArrayList of tasks in the column.
-     */
+    // Remove a task from the column's task list
+    public boolean removeTaskForColumn(String taskName) {
+        // Placeholder implementation
+        return false;
+    }
+
+    // Get the list of tasks in the column
     public ArrayList<Task> getColumnTaskList() {
+        // Placeholder implementation
         return columnTaskList;
     }
 
-    /**
-     * Returns the string representation of the column, which is its name.
-     *
-     * @return The name of the column.
-     */
     @Override
     public String toString() {
+        // Placeholder implementation
         return columnName;
     }
 }
