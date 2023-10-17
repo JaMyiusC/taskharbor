552685bbf243c8c9ffe996ace220b39f3870602b
Colin Mathis Elmore <colin_elmore@Colins-MacBook-Pro-3.local>
2023-10-17 08:50:35 -0400

    DataReader written

--- DataReader.java
+++ DataReader.java
@@ -1,28 +1,18 @@
 import java.util.ArrayList;
 
-/**
- * The DataReader class is responsible for reading and providing data related to users and projects.
- * It contains methods to retrieve lists of users and projects.
- */
 public class DataReader {
-    /**
-     * Retrieves a list of users from the data source.
-     *
-     * @return An ArrayList of User objects representing the users.
-     */
     public ArrayList<User> getUsers() {
-        // Implementation code to fetch and return the list of users
-        return null;
+        // Implement the logic to retrieve and return a list of User objects here
+        ArrayList<User> userList = new ArrayList<User>();
+        // Add User objects to the userList
+        return userList;
     }
 
-    /**
-     * Retrieves a list of projects from the data source.
-     *
-     * @return An ArrayList of Project objects representing the projects.
-     */
     public ArrayList<Project> getProjects() {
-        // Implementation code to fetch and return the list of projects
-        return null;
+        // Implement the logic to retrieve and return a list of Project objects here
+        ArrayList<Project> projectList = new ArrayList<Project>();
+        // Add Project objects to the projectList
+        return projectList;
     }
 }
 
