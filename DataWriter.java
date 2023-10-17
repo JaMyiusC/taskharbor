c217db81b8f60f052a9cd049478f17867c887275
Colin Mathis Elmore <colin_elmore@Colins-MacBook-Pro-3.local>
2023-10-17 09:09:09 -0400

    dataWriter shell

--- DataWriter.java
+++ DataWriter.java
@@ -1,25 +1,18 @@
-/**
- * The DataWriter class is responsible for saving user and project data to a data source.
- * It provides methods for saving user and project information.
- */
+import java.io.*;
+
+// The DataWriter class is responsible for saving user and project data to a data source.
 public class DataWriter {
-    /**
-     * Saves a list of users to the data source.
-     *
-     * @return true if the operation is successful, false otherwise.
-     */
+    // Method to save users. Returns true if the operation is successful, false otherwise.
     public boolean saveUsers() {
         // Implementation code to save the list of users
+        // This method currently returns false because the implementation is not provided.
         return false;
     }
 
-    /**
-     * Saves a list of projects to the data source.
-     *
-     * @return true if the operation is successful, false otherwise.
-     */
+    // Method to save projects. Returns true if the operation is successful, false otherwise.
     public boolean saveProjects() {
         // Implementation code to save the list of projects
+        // This method currently returns false because the implementation is not provided.
         return false;
     }
 }
