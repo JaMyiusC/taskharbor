115d1ee01032b3efd20c56348b49b2e0247d922e
TyrekBrunson <tyrekbrunson@aol.com>
2023-10-24 14:05:31 -0400

    updated the user class

--- User.java
+++ User.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.UUID;
 
@@ -41,7 +47,7 @@ public class User {
     }
 
 
-    // Constructor with random UUID generation
+    // Constructor for creating a new user with a random UUID
     public User(String firstName, String lastName, String userName, String password, String email, String phoneNumber, String birthday, String address) {
         this.id = UUID.randomUUID();
         this.firstName = firstName;
@@ -56,7 +62,7 @@ public class User {
         this.userHistory = new ArrayList<>();
     }
 
-    // Constructor with predefined UUID
+    // Constructor for creating a user from JSON data with a predefined UUID
     public User(UUID id, String firstName, String lastName, String userName, String password, String email, String phoneNumber, String birthday, String address) {
         this.id = id;
         this.firstName = firstName;
@@ -139,4 +145,40 @@ public class User {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+// Save user data to a JSON file
+public void saveToJson(String filePath) {
+    try (Writer writer = new FileWriter(filePath)) {
+        writer.write(this.toString());
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+
+// Load user data from a JSON file
+public static User loadFromJson(String filePath) {
+    try (Reader reader = new FileReader(filePath)) {
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        StringBuilder jsonContent = new StringBuilder();
+        String line;
+
+        while ((line = bufferedReader.readLine()) != null) {
+            jsonContent.append(line);
+        }
+
+        // Parse JSON content and create a User object
+        return parseJson(jsonContent.toString());
+    } catch (IOException e) {
+        e.printStackTrace();
+        return null;
+    }
+}
+
+
+private static User parseJson(String string) {
+    //for testing purposes only
+    return new User(UUID.randomUUID(), "Dummy", "User", "dummyuser", "dummyPassword", "dummy@example.com", "1234567890", "01-01-2000", "Dummy Address");
+}
+
+
 }
--- test.java
+++ test.java
@@ -1,32 +1,22 @@
-import java.util.UUID;
-
 public class test {
     public static void main(String[] args) {
-        // Test User creation with random UUID
-        User randomUser = new User("John", "Doe", "johndoe", "password123", "johndoe@example.com", "1234567890", "01-01-1990", "123 Street, City, Country");
-        System.out.println("Random User:\n" + randomUser.toString());
+        // Test creating a new user with a random UUID
+        User newUser = new User("Alice", "Johnson", "alicej", "password123", "alice@example.com", "9876543210", "1990-05-15", "456 Main St, City, Country");
+        System.out.println("New User:\n" + newUser.toString());
 
-        // Test User creation with predefined UUID
-        UUID predefinedUUID = UUID.fromString("f546c57a-47e8-4aa1-8ba8-d186d7a15637");
-        User predefinedUser = new User(predefinedUUID, "Jane", "Smith", "janesmith", "pass456", "janesmith@example.com", "9876543210", "02-02-1995", "456 Avenue, Town, Country");
-        System.out.println("\nPredefined User:\n" + predefinedUser.toString());
+        // Test saving user data to JSON
+        newUser.saveToJson("user_data.json");
+        System.out.println("User data saved to JSON.");
 
-        // Test password update
-        System.out.println("\nPassword check: " + predefinedUser.checkPassword("pass456"));
-        predefinedUser.updatePassword("newPass789");
-        System.out.println("Updated password check: " + predefinedUser.checkPassword("newPass789"));
-
-        // Test user confirmation
-        System.out.println("\nUser confirmation: " + predefinedUser.confirmUser("janesmith", "newPass789"));
-
-        // Test user task history
-        predefinedUser.addUserTaskHistory("Task 1 completed");
-        predefinedUser.addUserTaskHistory("Task 2 started");
-        System.out.println("\nUser task history:\n" + predefinedUser.viewUserTaskHistory());
-
-        // Test user column history
-        predefinedUser.addUserColumnHistory("Column 1 updated");
-        predefinedUser.addUserColumnHistory("Column 2 added");
-        System.out.println("\nUser column history:\n" + predefinedUser.viewUserColumnHistory());
+        // Test loading user data from JSON
+        User loadedUser = User.loadFromJson("user_data.json");
+        if (loadedUser != null) {
+            System.out.println("\nLoaded User:\n" + loadedUser.toString());
+        } else {
+            System.out.println("Failed to load user data from JSON.");
+        }
     }
 }
+
+
+
