8000e9b9a92dd2badb413051c3b9f04f40db7747
TyrekBrunson <tyrekbrunson@aol.com>
2023-11-08 15:01:09 -0500

    updating the datareadertestfile

---
+++ DataReaderTest.java
@@ -0,0 +1,53 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DataReaderTest {
+
+    @Before
+    public void setup() {
+        // Set up any necessary data or configurations before each test
+    }
+
+    @After
+    public void tearDown() {
+        // Clean up after each test
+    }
+
+    @org.junit.Test
+    public void testReadUsers() {
+        // Implement test for reading users from a file
+        // Ensure that the read data matches the expected data
+        // Use assertions to check the correctness of the data
+    }
+
+    @org.junit.Test
+    public void testReadTasks() {
+        // Implement test for reading tasks from a file
+        // Ensure that the read data matches the expected data
+        // Use assertions to check the correctness of the data
+    }
+
+    @org.junit.Test
+    public void testReadColumns() {
+        // Implement test for reading columns from a file
+        // Ensure that the read data matches the expected data
+        // Use assertions to check the correctness of the data
+    }
+
+    @org.junit.Test
+    public void testReadProjects() {
+        // Implement test for reading projects from a file
+        // Ensure that the read data matches the expected data
+        // Use assertions to check the correctness of the data
+    }
+
+    // Add more test cases as needed for different scenarios
+
+}
